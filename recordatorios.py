3e8f54c2f1510d81bda297a323532b7591866777
LiuvaSalas <liuvasalas01@gmail.com>
2024-08-08 20:28:07 -0400

    comente una linea que faltaba

--- recordatorios.py
+++ recordatorios.py
@@ -1,3 +1,4 @@
+#Generamos lista con recordatorios
 recordatorios = [['2021-01-01', "11:00", "Levantarse y ejercitar"],
  ['2021-05-01', "15:00", "No trabajar"],
  ['2021-07-15', "13:00", "No hacer nada es feriado"],
